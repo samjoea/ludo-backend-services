58bec65990a7b43968a21f0e2f27ca1ec29abf23
Joseph Sam <user.email>
2023-11-26 16:00:10 +0000

    update: peerjs-server

--- peerjs-server/server.ts
+++ peerjs-server/server.ts
@@ -4,9 +4,15 @@ const NODE_ENV = Bun.env.NODE_ENV ?? "development";
 const peerPort = Bun.env.PEER_PORT ?? 9000;
 
 const peerServer = PeerServer({
-	path: "/peerjs",
+	path: "/ludo-peerjs",
 	port: +peerPort,
+	allow_discovery: true,
+	corsOptions: '*',
 	proxied: true,
+});
+
+peerServer.on("connection", (client) => {
+	console.log("peerjs connection", client.getId());
 });
 
 console.log("peerjs running on port ", peerPort, ' on ', NODE_ENV);
